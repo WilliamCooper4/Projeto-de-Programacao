fbf6d02b5cd631cab9c4752bbd381a697d768541
P <194640994+escolaP@users.noreply.github.com>
2025-11-16 16:37:33 +0000

    Update Mockservertest.js

--- Mockservertest.js
+++ Mockservertest.js
@@ -12,11 +12,13 @@ user = {
 	lvl: 1,
 	exp: 0,
 	streak: 0,
-	reminders: {
-		"Test":{
-			time:1763310003,
-			class:"Computação Gráfica",
-			type:"idk"
-		}
-	}
+	lembretes: [
+		{ "id": 1, "texto": "Estudar para o teste de Matemática", "data": 1763310419 },
+		{ "id": 2, "texto": "Enviar trabalho de Português", "data": 1763297999 }
+	],
+
+	eventosCalendario: [
+		{ "id": 1, "titulo": "Aula de Química", "data": 2763310419 },
+		{ "id": 2, "titulo": "Apresentação", "data": 2767316419 }
+	]
 }
