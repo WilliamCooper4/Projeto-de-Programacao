a88c8c586acf58829ebb5752db889fad80e4459a
Paulogit24 <40240257@esmad.ipp.pt>
2025-11-20 14:43:54 +0000

    update

--- js/calendario.js
+++ js/calendario.js
@@ -1,1 +1,18 @@
+sidebar = document.getElementById('sidebar');
 testbtn = document.getElementById('testbtn');
+
+var isSidebarVisible = true;
+testbtn.addEventListener('click', function() {
+    if (isSidebarVisible===false){
+        sidebar.classList.remove("invisible");
+        console.log("change to visible");
+        isSidebarVisible=true;
+    }
+    else if (isSidebarVisible===true){
+        sidebar.classList.add("invisible");
+        console.log("change to invisible");
+        isSidebarVisible=false;
+    }
+});
+
+
