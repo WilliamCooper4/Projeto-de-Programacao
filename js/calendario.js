7f52f284f36db5ffa5e51522394e4b98ae182663
Paulogit24 <40240257@esmad.ipp.pt>
2025-11-23 16:09:18 +0000

    stuff

--- js/calendario.js
+++ js/calendario.js
@@ -1,5 +1,8 @@
 sidebar = document.getElementById('sidebar');
 testbtn = document.getElementById('testbtn');
+const UnitName = document.getElementById('name');
+const desc = document.getElementById('desc');
+const execise = document.getElementById('execise');
 
 var isSidebarVisible = true;
 testbtn.addEventListener('click', function() {
@@ -15,4 +18,10 @@ testbtn.addEventListener('click', function() {
     }
 });
 
+
+// Pegar dados da disciplina~
+
+UnitName.textContent = "Matemática Discreta";
+desc.textContent = "Estudo dos conceitos fundamentais da matemática que são aplicados na ciência da computação, incluindo lógica, teoria dos conjuntos, relações, funções, grafos e combinatória.";
+execise.textContent = "1. Prove que a soma dos primeiros n números naturais é dada pela fórmula n(n + 1)/2. \n 2. Determine o número de maneiras distintas de organizar os livros em uma prateleira. \n 3. Mostre que o conjunto dos números racionais é denso nos números reais.\n4. Resolva o problema do caixeiro viajante para um grafo completo com 5 vértices.\n5. Calcule o número de permutações de 7 elementos tomados 3 a 3.";
 
