a6568c63bf1b5d13023e6ee8c120fd0343b87c83
Paulogit24 <40240257@esmad.ipp.pt>
2025-12-21 15:46:03 +0000

    a

--- projeto programação/src/views/BaseView.vue
+++ projeto programação/src/views/BaseView.vue
@@ -81,61 +81,6 @@ window.showLogin = showLogin;
 
 <template>
 	<H1>THIS IS HOME</H1>
-
-  <div class="login-box , invisible" id="logbox">
-		<div class="modal-dialog modal-dialog-centered">
-			<div class="modal-content border-danger border-2 rounded-4">
-
-				<!-- Ecrã 1: Login -->
-				<div id="loginScreen" class="p-4">
-					<div class="text-center mb-4">
-						<a color="red">TRACKER</a>
-					</div>
-					<div class="mb-2">
-						<input type="text" id="username" class="form-control text-center rounded-pill"
-							placeholder="Nome de Utilizador">
-					</div>
-					<div class="mb-3">
-						<input type="password" id="password" class="form-control text-center rounded-pill"
-							placeholder="Palavra-passe">
-					</div>
-					<button id="btnLogin" class="btn btn-primary w-100 rounded-pill mb-2">Login</button>
-					<button class="btn btn-primary w-100 rounded-pill mb-3" onclick="showRegister()">Criar
-						conta</button>
-				</div>
-
-      <!-- Ecrã 2: Registo -->
-      <div id="registerScreen" class="p-4 d-none">
-        <button class="btn btn-link p-0 mb-3" onclick="showLogin()">← Voltar</button>
-        <div class="text-center mb-4">
-          <a>TRACKER</a>
-        </div>
-
-					<div class="mb-2"><input type="text" class="form-control text-center rounded-pill"
-							placeholder="Nome de Utilizador"></div>
-					<div class="mb-2"><input type="email" class="form-control text-center rounded-pill"
-							placeholder="E-mail"></div>
-					<div class="mb-2"><input type="email" class="form-control text-center rounded-pill"
-							placeholder="Confirmar E-mail"></div>
-					<div class="mb-2"><input type="password" class="form-control text-center rounded-pill"
-							placeholder="Palavra-passe"></div>
-					<div class="mb-3"><input type="password" class="form-control text-center rounded-pill"
-							placeholder="Confirmar Palavra-passe"></div>
-
-					<div class="form-check text-start mb-2 small">
-						<input class="form-check-input" type="checkbox" id="terms">
-						<label class="form-check-label" for="terms">
-							Ao criar conta, confirma que tem 18 anos ou mais e aceita os <a href="#">Termos</a> e a <a
-								href="#">Política</a>.
-						</label>
-					</div>
-
-					<button class="btn btn-primary w-100 rounded-pill">Criar conta</button>
-				</div>
-
-			</div>
-		</div>
-	</div>
 </template>
 
 <style scoped>
