b152743b7030cb346af557bd4a25d763be3db90e
Paulogit24 <40240257@esmad.ipp.pt>
2025-12-22 16:33:34 +0000

    a

--- projeto programação/src/views/DisciplinaView.vue
+++ projeto programação/src/views/DisciplinaView.vue
@@ -2,25 +2,6 @@
 import "../assets/Disciplina.css"
 import { onMounted } from 'vue'
     onMounted(() => {
-    const LogBTN = document.getElementById("Log")
-    const loginScreen= document.getElementById("logbox")
-        // deteta se o click é dentro da da logbox
-        window.addEventListener('click', function(e){
-            if (document.getElementById('logbox').contains(e.target)){
-                // Clicked in box
-                console.log("inside")
-            } else{
-                console.log("outside")
-                loginScreen.classList.toggle("invisible")
-            }
-        });
-
-    LogBTN.addEventListener("click", function() {
-        console.log("login")
-
-        loginScreen.classList.toggle("invisible")
-        })
-
 
     })
 
